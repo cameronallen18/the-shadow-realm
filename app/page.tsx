8c188c860fc74a4de573c6ff634e0092a72e5425
Cameron <cameronallen18@gmail.com>
2026-04-24 10:56:36 -0700

    feat(08): add Super Metroid sprite assets to public/sprites/

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           my digital junk drawer
         </p>
         <p className="text-[#ededed]/30 text-xs tracking-wide">
-          v0.1.0
+          v0.1.1
         </p>
       </div>
 
